5df55ac46a8c0622109fe6e6fa1f61a9d41f274a
hsuhong1210 <hhloh8332@gmail.com>
2018-10-27 04:58:46 +0800

    26 Oct 18 - HH

--- src/java/com/jsf/AssessmentTaskSetting.java
+++ src/java/com/jsf/AssessmentTaskSetting.java
@@ -34,11 +34,47 @@ public class AssessmentTaskSetting {
     private String cslevel, project, collaboration, practical, groupwork;
     private List<String> CSLevel_list = new ArrayList<>(); //CS level list that retrieve from db
 
-    private Boolean disabledDDL;
+    private Boolean disabledDDL, disabledProject, disabledCollaboration, disabledPractical, disabledGroupwork;
 
     public AssessmentTaskSetting() {
         this.year = 2018;
         this.disabledDDL = false;
+        this.disabledProject = false;
+        this.disabledCollaboration = false;
+        this.disabledPractical = false;
+        this.disabledGroupwork = false;
+    }
+
+    public Boolean getDisabledProject() {
+        return disabledProject;
+    }
+
+    public void setDisabledProject(Boolean disabledProject) {
+        this.disabledProject = disabledProject;
+    }
+
+    public Boolean getDisabledCollaboration() {
+        return disabledCollaboration;
+    }
+
+    public void setDisabledCollaboration(Boolean disabledCollaboration) {
+        this.disabledCollaboration = disabledCollaboration;
+    }
+
+    public Boolean getDisabledPractical() {
+        return disabledPractical;
+    }
+
+    public void setDisabledPractical(Boolean disabledPractical) {
+        this.disabledPractical = disabledPractical;
+    }
+
+    public Boolean getDisabledGroupwork() {
+        return disabledGroupwork;
+    }
+
+    public void setDisabledGroupwork(Boolean disabledGroupwork) {
+        this.disabledGroupwork = disabledGroupwork;
     }
 
     public int getYear() {
@@ -100,6 +136,37 @@ public class AssessmentTaskSetting {
         return disabledDDL;
     }
 
+//      //   change text box disabled when click the button
+//    public Boolean changeProjectDisabled() {
+//        
+//        disabledProject = changeDDLDisabled();
+//       
+//        return disabledProject;
+//    }
+//    
+//     //   change text box disabled when click the button
+//    public Boolean changeCollaborationDisabled() {
+//       
+//        disabledCollaboration = changeDDLDisabled();
+//       
+//        return disabledCollaboration;
+//    }
+//    
+//     //   change text box disabled when click the button
+//    public Boolean changePracticalDisabled() {
+//       
+//        disabledPractical = changeDDLDisabled();
+//       
+//        return disabledPractical;
+//    }
+//    
+//     //   change text box disabled when click the button
+//    public Boolean changeGroupworkDisabled() {
+//       
+//        disabledGroupwork = changeDDLDisabled();
+//       
+//        return disabledGroupwork;
+//    }
     //remove duplicate element for cs level id array
     public static int removeDuplicateElementsString(String arr[], int n) {
         if (n == 0 || n == 1) {
@@ -211,7 +278,7 @@ public class AssessmentTaskSetting {
 
         return CSLevel_list;
     }
-    
+
     //auto generate task ID
     public int autoGenerateID() {
 
@@ -238,17 +305,17 @@ public class AssessmentTaskSetting {
         return count;
     }
 
-    public String matchAssID(String CSID, String assActivityID){
-        
+    public String matchAssID(String CSID, String assActivityID, int tmpYear) {
+
         String assID = "";
-        
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stemcstmp1?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
             PreparedStatement st = con.prepareStatement("SELECT assID FROM assessment WHERE year = ? AND CSLevelID = ? AND assActivityID = ?");
-            st.setInt(1, year);
+            st.setInt(1, tmpYear);
             st.setString(2, CSID);
-             st.setString(3, assActivityID);
+            st.setString(3, assActivityID);
             ResultSet rs = st.executeQuery();
             while (rs.next()) {
                 // CSLevel_list.add(rs.getString("CSLevelID"));
@@ -261,11 +328,11 @@ public class AssessmentTaskSetting {
         } catch (Exception ex) {
             System.out.println("Error: " + ex);
         }
-        
+
         return assID;
     }
-    
-     public String matchCSLevelName(String cslevel, String assActivityID) {
+
+    public String matchCSLevelName(String cslevel, String assActivityID, int tmpYear) {
 
         String CSID = "";
 
@@ -286,39 +353,159 @@ public class AssessmentTaskSetting {
         } catch (Exception ex) {
             System.out.println("Error: " + ex);
         }
-        
-        return matchAssID(CSID, assActivityID);
+
+        return matchAssID(CSID, assActivityID, tmpYear);
 
     }
-     
-      //get assessment list when page onload and when button click based on year and cs level
+
+    public String matchCSLevelID() {
+
+        String CSID = "";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stemcstmp1?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+            PreparedStatement st = con.prepareStatement("SELECT CSLevelID FROM cslevel WHERE CSLevelName = ?");
+            st.setString(1, cslevel);
+            ResultSet rs = st.executeQuery();
+
+            while (rs.next()) {
+                CSID = rs.getString("CSLevelID");
+            }
+
+            st.close();
+            con.close();
+
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+        }
+
+        return CSID;
+    }
+
+    public String matchTaskTitle(String assID) {
+
+        String taskTitle = "";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stemcstmp1?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+            PreparedStatement st = con.prepareStatement("SELECT taskTitle FROM task WHERE assID = ?");
+            st.setString(1, assID);
+            ResultSet rs = st.executeQuery();
+
+            while (rs.next()) {
+                taskTitle = rs.getString("taskTitle");
+            }
+
+            st.close();
+            con.close();
+
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+        }
+
+        return taskTitle;
+    }
+
+    //get assessment list when page onload and when button click based on year and cs level
     public void assessmentList() {
 
         FacesContext context = FacesContext.getCurrentInstance();
 
         int tmpYear = 0, tmpyearComm;
         int numYearComm = 0;
-        
-        String csid = "", csname = "", yosid = "";
+
+        String csid = "", assactivityid = "", assidP = "", assid = "", tasktitle = "";
 
         //when page onload, need to show previous(2017) record, so for year and yearComm 2018 temporaily become 2017
         if (year == 2018) {
             tmpYear = 2017;
-          
+            disabledProject = false;
+            disabledCollaboration = false;
+            disabledPractical = false;
+            disabledGroupwork = false;
+
         } else {
             tmpYear = year;
+            disabledProject = true;
+            disabledCollaboration = true;
+            disabledPractical = true;
+            disabledGroupwork = true;
         }
+
+        //get project tasktitle
+        csid = matchCSLevelID();
+        assactivityid = "AA1";
+        assidP = matchAssID(csid, assactivityid, tmpYear);
+        assid = matchAssID(csid, assactivityid, year);
+        tasktitle = "";
+
+        if (!assid.isEmpty()) {
+            tasktitle = matchTaskTitle(assidP);
+        }else {
+                disabledProject = true;
+        }
+
+        project = tasktitle;
+
+        //get collaboration tasktitle
+        csid = matchCSLevelID();
+        assactivityid = "AA2";
+        assidP = matchAssID(csid, assactivityid, tmpYear);
+        assid = matchAssID(csid, assactivityid, year);
+        tasktitle = "";
+
+        if (!assid.isEmpty()) {
+            tasktitle = matchTaskTitle(assidP);
+        }else {
+                disabledCollaboration = true;
+        }
+
+        collaboration = tasktitle;
+
+        //get practical tasktitle
+        csid = matchCSLevelID();
+        assactivityid = "AA3";
+        assidP = matchAssID(csid, assactivityid, tmpYear);
+        assid = matchAssID(csid, assactivityid, year);
+        tasktitle = "";
+
+        if (!assid.isEmpty()) {
+            tasktitle = matchTaskTitle(assidP);
+        }else {
+                disabledPractical = true;
+        }
+
+        practical = tasktitle;
+
+        //get groupwork tasktitle
+        csid = matchCSLevelID();
+        assactivityid = "AA4";
+        assidP = matchAssID(csid, assactivityid, tmpYear);
+        assid = matchAssID(csid, assactivityid, year);
+        tasktitle = "";
+
+        if (!assid.isEmpty()) {
+            tasktitle = matchTaskTitle(assidP);
+        } else {
+                disabledGroupwork = true;
+        }
+
+        groupwork = tasktitle;
+
+        //   practical = csid + " : " + assactivityid + " : " + assid;
     }
-     
-       //add year of study cs map in db
+
+    //add year of study cs map in db
     public void addTask() { //if need redirect to another xhtml, need change void to String and return keyword
-        
-         FacesContext context = FacesContext.getCurrentInstance();
-        
-         int length = 0;
-         String tsID = "", assID = "", csID = "", assActivityID = "", assTypeID = "";
-        
-         try {
+
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        int length = 0;
+        String tsID = "", assID = "", csID = "", assActivityID = "", assTypeID = "";
+
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stemcstmp1?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
             PreparedStatement statement = (PreparedStatement) con.prepareStatement("INSERT INTO task (taskID, taskTitle, assID) VALUES (?, ?, ?)");
@@ -327,8 +514,7 @@ public class AssessmentTaskSetting {
             length = autoGenerateID();
             length = length + 1;
             tsID = "TS" + Integer.toString(length);
-            assID = matchCSLevelName(cslevel, "AA1");
-            
+            assID = matchCSLevelName(cslevel, "AA1", year);
 
             context.addMessage(null, new FacesMessage(tsID + " : " + assID));
 
@@ -336,13 +522,12 @@ public class AssessmentTaskSetting {
             statement.setString(2, project);
             statement.setString(3, assID);
             statement.executeUpdate();
-            
+
             //insert collaboration
             length = autoGenerateID();
             length = length + 1;
             tsID = "TS" + Integer.toString(length);
-            assID = matchCSLevelName(cslevel, "AA2");
-            
+            assID = matchCSLevelName(cslevel, "AA2", year);
 
             context.addMessage(null, new FacesMessage(tsID + " : " + assID));
 
@@ -350,13 +535,12 @@ public class AssessmentTaskSetting {
             statement.setString(2, collaboration);
             statement.setString(3, assID);
             statement.executeUpdate();
-            
-             //insert practical
+
+            //insert practical
             length = autoGenerateID();
             length = length + 1;
             tsID = "TS" + Integer.toString(length);
-            assID = matchCSLevelName(cslevel, "AA3");
-            
+            assID = matchCSLevelName(cslevel, "AA3", year);
 
             context.addMessage(null, new FacesMessage(tsID + " : " + assID));
 
@@ -364,13 +548,12 @@ public class AssessmentTaskSetting {
             statement.setString(2, practical);
             statement.setString(3, assID);
             statement.executeUpdate();
-            
-             //insert groupwork
+
+            //insert groupwork
             length = autoGenerateID();
             length = length + 1;
             tsID = "TS" + Integer.toString(length);
-            assID = matchCSLevelName(cslevel, "AA4");
-            
+            assID = matchCSLevelName(cslevel, "AA4", year);
 
             context.addMessage(null, new FacesMessage(tsID + " : " + assID));
 
@@ -378,7 +561,7 @@ public class AssessmentTaskSetting {
             statement.setString(2, groupwork);
             statement.setString(3, assID);
             statement.executeUpdate();
-            
+
             statement.close();
             con.close();
 

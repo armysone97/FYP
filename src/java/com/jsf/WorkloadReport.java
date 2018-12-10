1388f3362d83567f042f4003b7ace7997c7e28ef
ruenyen <chinry-pa15@student.tarc.edu.my>
2018-12-10 16:33:09 +0800

    Merge origin/master
    Conflicts:
    	web/GenerateReport.xhtml

--- src/java/com/jsf/WorkloadReport.java
+++ src/java/com/jsf/WorkloadReport.java
@@ -5,6 +5,9 @@
  */
 package com.jsf;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -17,10 +20,41 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 
 public class WorkloadReport {
+    private Connection con;
     private int counterReset;
+    
+    private List<String> year_list = new ArrayList<>();
+    private List<String> school_list = new ArrayList<>();
 
     public WorkloadReport() {
         this.counterReset = 0;
+    }
+
+    public void setSchool_list(List<String> school_list) {
+        this.school_list = school_list;
+    }
+
+    public void setYear_list(List<String> year_list) {
+        this.year_list = year_list;
+    }
+    
+    //display year
+    public List<String> get_YearList() {
+        year_list.clear();
+        
+        year_list.add("2016");
+        year_list.add("2017");
+        year_list.add("2018");
+        
+        return year_list;
+    }
+    
+    //retrive school list
+    public List<String> get_SchoolList() {
+        school_list.clear();
+        
+        
+        return school_list;
     }
 
     //navigation bar purpose
